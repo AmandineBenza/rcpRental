660e4d1db0f7395ac081236b7bff1b165fc42f59
Amandine Benza <amandine.benza@etu.unice.fr>
2020-11-03 17:04:40 +0100

    First step UI

--- com.sii.rental.ui/src/com/sii/rental/ui/parts/RentalPropertyPart.java
+++ com.sii.rental.ui/src/com/sii/rental/ui/parts/RentalPropertyPart.java
@@ -1,26 +1,54 @@
  
 package com.sii.rental.ui.parts;
 
-import javax.inject.Inject;
 import javax.annotation.PostConstruct;
+import javax.inject.Inject;
 
-import org.eclipse.swt.SWT;
-import org.eclipse.swt.widgets.Composite;
-import org.eclipse.swt.widgets.Label;
 import org.eclipse.e4.ui.di.Focus;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Group;
+import org.eclipse.swt.widgets.Label;
+
+import com.opcoach.training.rental.Rental;
+import com.sii.rental.core.RentalCoreActivator;
 
 public class RentalPropertyPart {
 	
 	
+	private Label rentedObjectlabel1;
+
+
 	@Inject
 	public RentalPropertyPart() {
 		
 	}
 	
 	@PostConstruct
-	public void postConstruct(Composite parent) {
+	public void createContent(Composite parent) {
 		Label label = new Label(parent, SWT.NONE);
 		label.setText("Hello");	
+		
+		parent.setLayout(new GridLayout(1, false));
+		
+		Group infoGroup = new Group(parent, SWT.NONE);
+		infoGroup.setText("Informations");
+		infoGroup.setLayout(new GridLayout(2, false));
+		
+		rentedObjectlabel1 = new Label(infoGroup, SWT.NONE);
+		GridData gd = new GridData();
+		gd.horizontalAlignment = SWT.FILL;
+		gd.horizontalSpan = 2;
+		rentedObjectlabel1.setLayoutData(gd);
+		setRental(RentalCoreActivator.getAgency().getRentals().get(0));
+	}
+	
+	public void setRental(Rental r) 
+	{
+		rentedObjectlabel1.setText(r.getRentedObject().getName());
+
 	}
 		
 	
