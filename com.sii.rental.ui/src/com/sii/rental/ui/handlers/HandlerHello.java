edbc42bf6f2902966cd2604b53994ba969b8e1cc
Amandine Benza <amandine.benza@etu.unice.fr>
2020-11-05 10:09:30 +0100

    Command Hello in main menu

---
+++ com.sii.rental.ui/src/com/sii/rental/ui/handlers/HandlerHello.java
@@ -0,0 +1,21 @@
+ 
+package com.sii.rental.ui.handlers;
+
+import javax.inject.Named;
+
+import org.eclipse.e4.core.di.annotations.Execute;
+import org.eclipse.e4.ui.services.IServiceConstants;
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.swt.widgets.Shell;
+
+public class HandlerHello {
+	
+	@Execute
+	public void execute(@Named(IServiceConstants.ACTIVE_SHELL) Shell shell) {
+		
+		MessageDialog.openInformation(shell, "Hello", "Hello there");
+		
+	}
+	
+		
+}
