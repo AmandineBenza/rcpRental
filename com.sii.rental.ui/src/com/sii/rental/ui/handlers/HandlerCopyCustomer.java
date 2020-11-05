79e4d12d5d5aa47f733e390f51d7400f956bab39
Amandine Benza <amandine.benza@etu.unice.fr>
2020-11-05 11:53:28 +0100

    Add command copy with filter on customer type

---
+++ com.sii.rental.ui/src/com/sii/rental/ui/handlers/HandlerCopyCustomer.java
@@ -0,0 +1,43 @@
+ 
+package com.sii.rental.ui.handlers;
+
+import javax.inject.Named;
+
+import org.eclipse.e4.core.di.annotations.CanExecute;
+import org.eclipse.e4.core.di.annotations.Evaluate;
+import org.eclipse.e4.core.di.annotations.Execute;
+import org.eclipse.e4.ui.services.IServiceConstants;
+import org.eclipse.swt.dnd.Clipboard;
+import org.eclipse.swt.dnd.RTFTransfer;
+import org.eclipse.swt.dnd.TextTransfer;
+import org.eclipse.swt.dnd.Transfer;
+import org.eclipse.swt.widgets.Display;
+
+import com.opcoach.training.rental.Customer;
+
+public class HandlerCopyCustomer {
+	
+	@CanExecute
+	@Evaluate
+	public boolean evaluate(@Named(IServiceConstants.ACTIVE_SELECTION) Object o) 
+	{
+		return o instanceof Customer;
+		
+	}
+
+	@Execute
+	public void execute(Display display, @Named(IServiceConstants.ACTIVE_SELECTION) Customer customer) {
+	
+	 	Clipboard clipboard = new Clipboard(display);
+		String textData = customer.getDisplayName();
+		String rtfData = "{\\rtf1\\b\\i " + textData + "}";
+		TextTransfer textTransfer = TextTransfer.getInstance();
+		RTFTransfer rtfTransfer = RTFTransfer.getInstance();
+		Transfer[] transfers = new Transfer[]{textTransfer, rtfTransfer};
+		Object[] data = new Object[]{textData, rtfData};
+		clipboard.setContents(data, transfers);
+		clipboard.dispose();
+		
+	}
+		
+}
