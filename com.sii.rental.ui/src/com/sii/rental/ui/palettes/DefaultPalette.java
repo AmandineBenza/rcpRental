11f4e791f974f19988d9d3f1dde7f4f80cc04845
Amandine Benza <amandine.benza@etu.unice.fr>
2020-11-06 12:08:47 +0100

    Definition extension point

---
+++ com.sii.rental.ui/src/com/sii/rental/ui/palettes/DefaultPalette.java
@@ -0,0 +1,24 @@
+package com.sii.rental.ui.palettes;
+
+import org.eclipse.jface.viewers.IColorProvider;
+import org.eclipse.swt.graphics.Color;
+
+public class DefaultPalette implements IColorProvider {
+
+	public DefaultPalette() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public Color getForeground(Object element) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Color getBackground(Object element) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
